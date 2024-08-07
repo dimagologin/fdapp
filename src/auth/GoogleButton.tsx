c5a0dfba9242178b797da6a881d4895cf4377f84
Dmitry Kaigorodov <dima.kaigorodov@gmail.com>
2024-08-07 13:14:52 +0200

    jwt-decode

--- src/auth/GoogleButton.tsx
+++ src/auth/GoogleButton.tsx
@@ -1,10 +1,12 @@
 import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from 'jwt-decode';
 
 export function GoogleButton() {
   return <div className='inline-block py-4'>
     <GoogleLogin
       onSuccess={credentialResponse => {
         console.log(credentialResponse);
+        console.log(jwtDecode(credentialResponse.credential || ""));
       }}
       onError={() => {
         console.error('Login Failed');
