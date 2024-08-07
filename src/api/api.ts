1b808ee32496d5f35489aa692841bed3a122aefb
Dmitry Kaigorodov <dima.kaigorodov@gmail.com>
2024-08-07 14:07:34 +0200

    fix httpS api reqs @ fdapp.pages.dev

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,9 @@
 export const isLocalhost = window.location.hostname === 'localhost';
-export const isProd = window.location.hostname === 'floppydata.com';
+export const isProd =
+  window.location.hostname === 'floppydata.com' ||
+  window.location.hostname === 'fdapp.pages.dev' // because staging does not work with httpS
+  ;
+
 const base = isProd
   ? 'https://api.floppydata.com/v1/'
   : 'http://139.162.187.132:10082/v1/';
